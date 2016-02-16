f55d0a7cf0cbeca436f4a853c1d28fcf22b7f886
hulsea <andre.hulse@vizexplorer.com>
2016-02-16 21:48:12 +1300

    Added a second spec to demo testing in parallel on the grid with both FF and Chrome

--- tests/e2e/conf.js
+++ tests/e2e/conf.js
@@ -3,25 +3,35 @@ var jasmineReporters = require('jasmine-reporters');
 
 var reporter = new HtmlScreenshotReporter({
 		dest : 'target/testresults',
-		filename : 'test-report.html'
+		filename : 'test-report.html',
+		showConfiguration : false,
+		reportTitle : null
 	});
 
 exports.config = {
 
 	framework : 'jasmine2',
-	
-	seleniumServerJar: '../../node_modules/protractor/selenium/selenium-server-standalone-2.40.0.jar',
-	seleniumPort: null,
-	chromeDriver: '../../node_modules/protractor/selenium/chromedriver',
-	
+
+	seleniumServerJar : '../../node_modules/protractor/selenium/selenium-server-standalone-2.40.0.jar',
+	seleniumPort : null,
+	chromeDriver : '../../node_modules/protractor/selenium/chromedriver',
+
 	//If seleniumAddress is set, seleniumServerJar, seleniumPort and chromeDriver settings will be ignored
 	//and tests will be ran in an already running instance of Selenium server, such as our internal Grid.
 	//Alternatively, this can be set by command line using $ grunt --seleniumAddress http://192.168.99.100:4444/wd/hub
-	//seleniumAddress: 'http://192.168.99.100:4444/wd/hub'	
+	seleniumAddress: 'http://192.168.99.100:4444/wd/hub',
 
-	capabilities : {
-		browserName : 'chrome'
-	},
+	multiCapabilities : [{
+			'browserName' : 'chrome',
+			maxInstances : 2,
+			shardTestFiles : true
+		}, {
+			'browserName' : 'firefox',
+			maxInstances : 2,
+			shardTestFiles : true
+		}
+	],
+	maxSessions : 4,
 
 	// Setup the report before any tests start
 	beforeLaunch : function () {
@@ -42,7 +52,7 @@ exports.config = {
 						return config.capabilities.browserName + '.' + generatedSuiteName;
 					}
 				});
-			jasmine.getEnv().addReporter(junitReporter);			
+			jasmine.getEnv().addReporter(junitReporter);
 		});
 	},
 
